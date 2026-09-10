90fcdde5d53cf26f63887c669f17045bcf0d32a4
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-09-10 09:35:33 +0800

    lineage-23.2: 20260910

--- lineage-23.2/mojito.sh
+++ lineage-23.2/mojito.sh
@@ -8,10 +8,12 @@ git clone -b 16.2 https://github.com/ximi-mojito-test/android_vendor_xiaomi_moji
 
 git clone -b 16.2 https://github.com/ximi-mojito-test/android_vendor_xiaomi_sm6150-common --depth=1 vendor/xiaomi/sm6150-common
 
-git clone -b lineage-23.2-tweaked https://github.com/ximi-daisy-test/android_system_logging/ --depth=1 system/logging
+git clone -b lineage-23.2-tweaked https://github.com/ximi-daisy/android_system_logging/ --depth=1 system/logging
 
-git clone -b lineage-23.2-tweaked https://github.com/ximi-daisy-test/android_bootable_recovery --depth=1 bootable/recovery
+git clone -b lineage-23.2-tweaked https://github.com/ximi-daisy/android_bootable_recovery --depth=1 bootable/recovery
 
-git clone -b lineage-23.2-tweaked https://github.com/ximi-daisy-test/android_vendor_lineage --depth=1 vendor/lineage
+git clone -b lineage-23.2-tweaked https://github.com/ximi-daisy/android_vendor_lineage --depth=1 vendor/lineage
+
+# git clone -b lineage-23.2-tweaked https://github.com/ximi-daisy/android_device_lineage_sepolicy/ --depth=1 device/lineage/sepolicy
 
 git clone -b lineage-23.2 https://github.com/LineageOS/android_hardware_xiaomi --depth=1 hardware/xiaomi
